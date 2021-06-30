b63332343765500d412e789d509329d2dd930bc3
Aysun Molla <aysun.molla@vestel.com.tr>
2021-06-30 16:25:51 +0300

    Initial Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by Aysun Molla on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
